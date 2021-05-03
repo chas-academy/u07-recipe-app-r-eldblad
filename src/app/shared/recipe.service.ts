60bcf785926dd1556a08ebbaf301a3a0de0d67b0
Rasmus Eldblad <r.eldblad@protonmail.com>
2021-05-03 22:55:19 +0200

    This commit adds environment variables

--- src/app/shared/recipe.service.ts
+++ src/app/shared/recipe.service.ts
@@ -2,14 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { shareReplay } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+
 @Injectable({
   providedIn: 'root',
 })
 export class RecipeService {
   recipes: any = [];
-  constructor(private http: HttpClient) {}
-  apiKey: string = 'b492684e487f4ca9a1e8a11b288e4239';
+  apiKey: string;
+  constructor(private http: HttpClient) {
+    this.apiKey = environment.API_KEY;
+  }
+
   recipeUrl: string = `https://api.spoonacular.com/recipes/random?number=20&apiKey=${this.apiKey}`;
+
   getRecipesFromApi(): Observable<any[]> {
     return this.http.get<any[]>(this.recipeUrl);
   }
