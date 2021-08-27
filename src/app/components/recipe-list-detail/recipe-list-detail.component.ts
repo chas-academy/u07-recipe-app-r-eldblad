3f668dd1dfc6c664dbf77e35c9fb73c7f0ea9156
Rasmus Eldblad <r.eldblad@protonmail.com>
2021-08-27 16:53:10 +0200

    This commit attempts to fix a issues i had with user not being able to delete recipe from recipe list

--- src/app/components/recipe-list-detail/recipe-list-detail.component.ts
+++ src/app/components/recipe-list-detail/recipe-list-detail.component.ts
@@ -18,6 +18,7 @@ export class RecipeListDetailComponent implements OnInit {
   updatedRecipes: any = [];
   updatedRecipeIds: any = [];
   subscription: Subscription;
+  newSet: any = [];
 
   constructor(
     private recipeListService: RecipeListService,
@@ -27,27 +28,46 @@ export class RecipeListDetailComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    // "[663559,1000566,665744,653445,646738]"
     this.recipeListService
       .getRecipeList(this.recipeListId)
       .subscribe((list) => {
-        this.recipeList = list;
+        this.recipeList = list; // detta skulle man console.log:at, kopierat ut i en konstant och lagt i projektet.
         this.recipeList.recipe_ids = JSON.parse(this.recipeList.recipe_ids);
         this.recipeIds = this.recipeList.recipe_ids;
         this.recipeIds.forEach((element: number) => {
           this.recipeService.getRecipe(element).subscribe((data) => {
-            this.recipes.push(data);
+            this.recipes.push(data); // samma med detta.
+            console.log(data);
           });
         });
-        console.log(this.recipes);
+        //console.log(this.recipes);
       });
   }
 
   onDelete(recipeId: number) {
-    this.updatedRecipes = this.recipes.filter((item) => item.id !== recipeId);
-    this.updatedRecipes.forEach((element) => {
-      this.updatedRecipeIds.push(element.id);
+    console.log(`b4: ${this.recipes}`);
+     
+     // gamla: this.updatedRecipes = this.recipes.filter(
+      this.updatedRecipes = this.recipeList.recipe_ids.filter(
+      (item: any) => item.id !== recipeId
+    );
+    //this.updatedRecipes = this.recipes.filter((item: any) => console.log(item));
+    console.log(`after filter: ${this.updatedRecipes}`);
+    this.updatedRecipes.forEach((element: any) => {
+      if (this.updatedRecipeIds === null) {
+        JSON.parse(this.updatedRecipeIds).push(element.id);
+      }
     });
     console.log(this.updatedRecipeIds);
+    this.recipeListService
+      .updateList(
+        this.recipeList.title,
+        JSON.stringify(this.updatedRecipeIds),
+        this.recipeListId
+      )
+      .subscribe();
+    this.router.navigate(['/recipe-list-detail/{{recipeListId}}']);
   }
 
   onSelect(userRecipe: number) {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,3 @@
-
 export const environment = {
   production: false,
   API_KEY: 'b492684e487f4ca9a1e8a11b288e4239',
