2adccb5bb09f99718b48c8fde8d9e098f06702b0
Rasmus Eldblad <r.eldblad@protonmail.com>
2021-08-30 19:05:48 +0200

    added env variables for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
   production: true,
+  // Spoonacular API
+  API_KEY: process.env.API_KEY,
+  // User Requests
+  REGISTER_USER: process.env.REGISTER_USER,
+  SIGN_IN: process.env.SIGN_IN,
+  PROFILE_USER: process.env.PROFILE_USER,
+
+  // CRUD Requests
+  GET_ALL_RECIPE_LISTS: process.env.GET_ALL_RECIPE_LISTS,
+  GET_RECIPE_LIST: process.env.GET_RECIPE_LIST,
+  CREATE_RECIPE_LIST: process.env.CREATE_RECIPE_LIST,
+  UPDATE_RECIPE_LIST: process.env.UPDATE_RECIPE_LIST,
+  DELETE_RECIPE_LIST: process.env.DELETE_RECIPE_LIST,
 };
