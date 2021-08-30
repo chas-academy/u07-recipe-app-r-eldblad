93fcbb746cec02afbee23f305e740832db567a56
Rasmus Eldblad <r.eldblad@protonmail.com>
2021-08-30 16:30:11 +0200

    reset env variables once again

--- src/app/shared/recipe-list.service.ts
+++ src/app/shared/recipe-list.service.ts
@@ -7,11 +7,11 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class RecipeListService {
-  getAllRecipeLists = process.env.GET_ALL_RECIPE_LISTS;
-  fetchRecipeList = process.env.GET_RECIPE_LIST;
-  createListUrl = process.env.CREATE_RECIPE_LIST;
-  deleteListUrl = process.env.DELETE_RECIPE_LIST;
-  updateListUrl = process.env.UPDATE_RECIPE_LIST;
+  getAllRecipeLists = environment.GET_ALL_RECIPE_LISTS;
+  fetchRecipeList = environment.GET_RECIPE_LIST;
+  createListUrl = environment.CREATE_RECIPE_LIST;
+  deleteListUrl = environment.DELETE_RECIPE_LIST;
+  updateListUrl = environment.UPDATE_RECIPE_LIST;
 
   constructor(private http: HttpClient) {}
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,25 @@
 export const environment = {
   production: false,
+  API_KEY: 'b492684e487f4ca9a1e8a11b288e4239',
+
+  // User Requests
+  REGISTER_USER:
+    'https://u08-recipe-api-r-eldblad.herokuapp.com/api/auth/register',
+  SIGN_IN: 'https://u08-recipe-api-r-eldblad.herokuapp.com/api/auth/login',
+  PROFILE_USER:
+    'https://u08-recipe-api-r-eldblad.herokuapp.com/api/auth/user-profile',
+
+  // CRUD Requests
+  GET_ALL_RECIPE_LISTS:
+    'https://u08-recipe-api-r-eldblad.herokuapp.com/api/auth/recipe-lists',
+  GET_RECIPE_LIST:
+    'https://u08-recipe-api-r-eldblad.herokuapp.com/api/auth/recipe-lists',
+  CREATE_RECIPE_LIST:
+    'https://u08-recipe-api-r-eldblad.herokuapp.com/api/auth/recipe-lists',
+  UPDATE_RECIPE_LIST:
+    'https://u08-recipe-api-r-eldblad.herokuapp.com/api/auth/recipe-lists',
+  DELETE_RECIPE_LIST:
+    'https://u08-recipe-api-r-eldblad.herokuapp.com/api/auth/recipe-lists',
 };
 /*
 export const environment = {
