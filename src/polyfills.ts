58952cf979e1da491b915c11f55569bc55a8d849
Rasmus Eldblad <r.eldblad@protonmail.com>
2021-08-30 19:15:00 +0200

    This commit fixes issues where process was not defined

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -57,8 +57,9 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js';  // Included with Angular CLI.
-
+import 'zone.js'; // Included with Angular CLI.
+import * as process from 'process';
+window['process'] = process;
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
